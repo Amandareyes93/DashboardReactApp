00f972f431c395e4398c58018827a52484e72051
Amanda Reyes <amanda_reyesb@hotmail.com>
2023-03-16 21:25:08 +0000

    navbar finished

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,5 +1,5 @@
 import React, { useEffect } from 'react';
-import { AiOutlineMenu }from 'react-icons/ai';
+import { AiOutlineMenu } from 'react-icons/ai';
 import { FiShoppingCart } from 'react-icons/fi';
 
 import { BsChatLeft } from 'react-icons/bs';
@@ -21,17 +21,35 @@ const NavButton = ({ title, customFunc, icon, color, dotColor }) => (
       <span
         style={{ background: dotColor }}
         className="absolute inline-flex rounded-full h-2 w-2 right-2 top-2"
-      >
-        {icon}
-      </span>
+      />
+      {icon}
     </button>
   </TooltipComponent>
 );
 
-const handleClick = () => {};
-
 function Navbar() {
-  const { activeMenu, setActiveMenu, isClicked, setIsClicked } = useStateContext();
+  const { activeMenu, setActiveMenu, isClicked, setIsClicked, handleClick, screenSize, setScreenSize } =
+    useStateContext();
+
+  // Aqui guarda en screenSize el ancho de la pantalla al inicio de la app
+  useEffect(() => {
+    const handleResize = () => setScreenSize(window.innerWidth);
+
+    window.addEventListener('resize', handleResize);
+
+    handleResize();
+
+    return () => window.removeEventListener('resize', handleResize);
+  }, []);
+
+  // Aqui cada vez que cambie el screenSize, se pregunta si activar o no el menu
+  useEffect(() => {
+    if (screenSize <= 900) {
+      setActiveMenu(false);
+    } else {
+      setActiveMenu(true);
+    }
+  }, [screenSize]);
 
   return (
     <div className="flex justify-between p-2 md:mx-6 relative">
@@ -66,22 +84,24 @@ function Navbar() {
           color="blue"
         />
 
-        <TooltipComponent content='Profile' position ="BottomCenter">
-          <div className='flex items-center gap-2 cursor-pointer p-1 hover:bg-light-gray rounded-lg' onClick={()=>handleClick('userProfile')}>
-              <img src={avatar} alt="you smiling" className='rounded-full w-8 h-8' />
-              <p>
-                <span className='text-gray-400 text -14'>Hi, </span> {' '}
-                <span className='text-gray-400 font-bold ml-1 text-14'>Michael</span>
-              </p>
-              <MdKeyboardArrowDown className='text-gray-400 text-14'/>
+        <TooltipComponent content="Profile" position="BottomCenter">
+          <div
+            className="flex items-center gap-2 cursor-pointer p-1 hover:bg-light-gray rounded-lg"
+            onClick={() => handleClick('userProfile')}
+          >
+            <img src={avatar} alt="you smiling" className="rounded-full w-8 h-8" />
+            <p>
+              <span className="text-gray-400 text -14">Hi, </span>{' '}
+              <span className="text-gray-400 font-bold ml-1 text-14">Michael</span>
+            </p>
+            <MdKeyboardArrowDown className="text-gray-400 text-14" />
           </div>
-
         </TooltipComponent>
 
-        {isClicked.cart && <Cart/>}
-        {isClicked.chat && <Chat/>}
-        {isClicked.notification && <Notification/>}
-        {isClicked.UserProfile && <UserProfile/>}
+        {isClicked.cart && <Cart />}
+        {isClicked.chat && <Chat />}
+        {isClicked.notification && <Notification />}
+        {isClicked.userProfile && <UserProfile />}
       </div>
     </div>
   );
--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -10,7 +10,13 @@ import { useStateContext } from '../contexts/ContextProvider';
 
 function Sidebar() {
 
-  const { activeMenu, setActiveMenu } = useStateContext();
+  const { activeMenu, setActiveMenu, screenSize } = useStateContext();
+
+  const handleCloseSidebar = ()=>{
+    if(activeMenu && screenSize <= 900) {
+      setActiveMenu(false)
+    }
+  }
 
   const activeLink = 'flex items-center gap-5 pl-4 pt-3 pb-2.5 rounded-lg text-white text-md m-2';
   const normalLink= 'flex items-center gap-5 pl-4 pt-3 pb-2.5 rounded-lg text-md text-gray-500 dark:text-gray-200 dark:hover:text-black hover:bg-light-gray m-2';
@@ -21,7 +27,7 @@ function Sidebar() {
           <div className="flex justify-between items-center">
             <Link
               to="/"
-              onClick={() => setActiveMenu(false)}
+              onClick={handleCloseSidebar}
               className="items-center gap-3 ml-3 mt-4 flex text-xl font-extrabold tracking-tight dark:text-white text-slate-900"
             >
               <SiShopware />
@@ -41,7 +47,7 @@ function Sidebar() {
                   {item.title}  
                 </p>
                 {item.links.map((link)=>
-                <NavLink to={`/${link.name}`} key={link.name} onClick={()=>{}} className={({ isActive })=> isActive ? activeLink : normalLink}>
+                <NavLink to={`/${link.name}`} key={link.name} onClick={handleCloseSidebar} className={({ isActive })=> isActive ? activeLink : normalLink}>
                   {link.icon}
                   <span className='capitalize'>{link.name}</span>
                 </NavLink>
